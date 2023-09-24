47ae543804528280e696ff1bd8332f1b3c617044
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2023-09-23 19:14:01 -0500

    yes

---
+++ schedulebuilder.js
@@ -0,0 +1,13 @@
+class scheduleBuilder {
+    constructor(){
+        this.remainingCourses = null;
+    }
+
+    buildSchedule = (remainingCourses) => {
+        console.log('schedule');
+        // call DB for geneds
+        // call DB for cores
+    }
+}
+
+module.exports = scheduleBuilder;
